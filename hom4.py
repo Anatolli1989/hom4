c8ca358e22b0139b5593c4d4d3f576af7fddabd0
DESKTOP-E6BUTSB\Анатолий <tolya.ponomarenko.89@ukr.net>
2024-01-16 13:26:08 +0100

    hom4

---
+++ hom4.py
@@ -0,0 +1,53 @@
+# string = input('Enter a string: ')
+#
+# letter_count = 0
+#
+# digit_count = 0
+#
+# for char in string:
+#
+#    if char.isalpha():
+#
+#        letter_count += 1
+#
+#    elif char.isdigit():
+#
+#        digit_count += 1
+#
+# print('Number of letters:', letter_count)
+#
+# print('Number of digits:', digit_count)
+
+# text = "Homework number four"
+# numbers = "2325"
+#
+# print(text.find("Homework number four"))  # 0
+# print(text.find("r"))  # 2
+# print(text.rfind("r"))  # 9
+#
+# print(numbers.find("2325"))  # 0
+# print(numbers.find("2"))  # 2
+# print(numbers.rfind("2"))
+
+#
+# original_string = input("Enter the original string: ")
+#
+# word_to_replace = input("Enter the word to replace: ")
+#
+# replacement_word = input("Enter the replacement word: ")
+#
+# modified_string = original_string.replace(word_to_replace, replacement_word)
+#
+# print("Modified string: " + modified_string)
+
+
+# sentence = input("Homework number four")
+# print(sentence[2])
+# print(sentence[-2])
+# print(sentence[:5])
+# print(sentence[:-2])
+# print(sentence[::2])
+# print(sentence[1::2])
+# print(sentence[::-1])
+# print(sentence[::-2])
+# print(len(sentence))
